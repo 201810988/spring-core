58d4269fadb08f2674358a8b064200063d3805f9
hyuk <201810988@sangmyung.kr>
2021-11-14 17:01:51 +0900

    second

--- src/main/kotlin/com/asada/sinon/AppConfig.kt
+++ src/main/kotlin/com/asada/sinon/AppConfig.kt
@@ -6,31 +6,41 @@ import com.asada.sinon.discount.RateDiscountPolicy
 import com.asada.sinon.member.*
 import com.asada.sinon.order.OrderService
 import com.asada.sinon.order.OrderServiceImpl
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class AppConfig {
 
+    @Bean
     fun memberService(): MemberService = MemberServiceImpl(memberRepository())
 
+    @Bean
     fun orderService(): OrderService = OrderServiceImpl(
         memberRepository(),
         fixDiscountPolicy()
     )
 
+    @Bean
     fun discountPolicy(): DiscountPolicy = RateDiscountPolicy()
 
+    @Bean
     fun memberRepository(): MemberRepository = MemoryMemberRepository()
 
+    @Bean
     fun fixDiscountPolicy(): DiscountPolicy = FixDiscountPolicy()
 }
 
 fun main(args: Array<String>) {
-    val appConfig = AppConfig()
 
-    val memberService = appConfig.memberService()
+    val applicationContext = AnnotationConfigApplicationContext(AppConfig::class.java)
+    val memberService = applicationContext.getBean("memberService", MemberService::class.java)
+
     val member = Member(1L, "memberA", Grade.VIP)
     memberService.join(member)
 
-    val orderService = appConfig.orderService()
+    val orderService = applicationContext.getBean("orderService", OrderService::class.java)
     val order = orderService.createOrder(1L, "itemA", 10000)
 
     println("order = $order")
